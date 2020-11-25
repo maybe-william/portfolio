50ecdf19a212f362301a54e7a94530b9cdddad01
william <191marquis@gmail.com>
2020-11-25 02:40:02 -0500

    [DEV] Finish up the site description text

--- src/About/About.js
+++ src/About/About.js
@@ -3,19 +3,15 @@ import Box from '../Box/Box';
 function About() {
     return (
         <Box anchor="about" title="About">
-        <p>
-        About About About About About About
-        About About About About About About
-        About About About About About About
-        About About About About About About
-        About About About About About About
-        About About About About About About
-        About About About About About About
-        About About About About About About
-        About About About About About About
-        About About About About About About
-        About About About About About About
-        About About About About About About
+        <p style={{fontSize: "18px"}}>
+        I’m a Software Engineer. The most recent project I’ve worked on is the LoveBank project described here. In terms of proficiency, I have played around with a large handful of different languages, but my most proficient are higher-level languages such as Ruby, Python, or Javascript. I can also program well in C, although I do prefer the extra freedom that higher-level languages grant. I also tend towards the Functional Programming paradigm.
+        <br />
+        <br />
+        I started out as a farmer, strangely enough. Additionally, I have been both a welder and an english professor in the past. In terms of programming, I’ve been self-learning in the field for 10 years. For the last one and a half years, I’ve been at Holberton School of Software Engineering after having made the decision to go there in order to improve my professional skills as well as to increase my network.
+        <br />
+        <br />
+        I am motivated by elegant and working solutions. Quite often, that means I’m driven to find creative solutions to unusual problems. As a part of a team, I am unafraid of thinking laterally and challenging assumptions in order to view a problem from many diverse perspectives instead of simply through the lens of an accepted solution or methodology.
+
         </p>
         </Box>
     );
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -2,13 +2,17 @@ import Box from '../Box/Box';
 import Project from './Project';
 
 function Projects(props) {
+    const loveBankDesc = "LoveBank is a mobile application with the goal of helping couples improve their relationship. Tasks are requested by one partner, with points being awarded upon their completion. The backend is written using Google Firebase and cloud functions, while the frontend is written using Flutter. This project was made with a team of ten other programmers using an agile methodology.";
+    const rocketRidersDesc = "Rocket Riders is a game fashioned in the style of old fashioned space shoot 'em up games such as Galaga. The game features music, immersive 2D graphics, smooth gameplay, and a sense of nostalgia. This project was made with two other team members as the final project for Holberton School's foundational year. Rocket Riders is written using the Phaser3 game library for Javascript.";
+    const leveledLootDesc = "LeveledLoot is a small minecraft mod that increases the amount of loot gained from mobs and from blocks as the player's level increases. This adds a sense of progression to the game, specifically dealing with the collection of materials. The mod is written using the Forge Mod Loader API, Gradle as a build system, and using Scala as the development language.";
+    const visualizerDesc = "Clockwork Coders Visualizer was my first program written as part of the Clockwork Coders development group. It would take a sound file, analyze it's component parts using the Discrete Fourier Transform, generate an image based on the strength of different frequencies, and then save that image as a png file. It was written in Java and made use of the Swing GUI library. All of us in the development group were very proud to have completed this program. Sadly, it has been lost to the sands of time.";
 
     return (
         <Box anchor="projects" title="Projects">
-        <Project title="LoveBank" projectLink="https://github.com/GittieLabs/lovebank-frontend" projectImage={process.env.PUBLIC_URL + '/img/LoveBank.png'} projectDescription="Description Description Description Description Description Description Description Description Description" />
-        <Project title="Rocket Riders" projectLink="https://github.com/maybe-william/RocketRiders" projectImage={process.env.PUBLIC_URL + '/img/RocketRiders.jpg'} projectDescription="Description Description Description Description Description Description Description Description Description" />
-        <Project title="LeveledLoot" projectLink="https://github.com/maybe-william/LeveledLoot" projectImage={process.env.PUBLIC_URL + '/img/LeveledLoot.png'} projectDescription="Description Description Description Description Description Description Description Description Description" />
-        <Project title="Clockwork Coders Visualizer" projectLink="" projectImage={process.env.PUBLIC_URL + '/img/Visualizer.jpg'} projectDescription="Description Description Description Description Description Description Description Description Description" />
+        <Project title="LoveBank" projectLink="https://github.com/GittieLabs/lovebank-frontend" projectImage={process.env.PUBLIC_URL + '/img/LoveBank.png'} projectDescription={loveBankDesc} />
+        <Project title="Rocket Riders" projectLink="https://github.com/maybe-william/RocketRiders" projectImage={process.env.PUBLIC_URL + '/img/RocketRiders.jpg'} projectDescription={rocketRidersDesc} />
+        <Project title="LeveledLoot" projectLink="https://github.com/maybe-william/LeveledLoot" projectImage={process.env.PUBLIC_URL + '/img/LeveledLoot.png'} projectDescription={leveledLootDesc} />
+        <Project title="Clockwork Coders Visualizer" projectLink="" projectImage={process.env.PUBLIC_URL + '/img/Visualizer.jpg'} projectDescription={visualizerDesc} />
         </Box>
     );
 }
